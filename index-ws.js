ad3e49c60e2317129dca9ab06187107a4748177e
Serhat Can <serhatcan1996@gmail.com>
2026-08-04 00:13:41 +0200

    feat: add simple sqlite db

--- index-ws.js
+++ index-ws.js
@@ -14,6 +14,18 @@ server.listen(3000,
     console.log('Server started on port 3000')
   })
 
+process.on('SIGINT', () =>
+{
+  wss.clients.forEach(function each(client)
+  {
+    client.close()
+  })
+
+  server.close(() =>
+  {
+    shutdownDB()
+  })
+})
 //  Begin websocket
 
 const WebSocketServer = require('ws').Server
@@ -32,6 +44,10 @@ wss.on('connection', function connection(ws)
     ws.send('Welcome to my server')
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+    `)
+
   ws.on('close', function close()
   {
     wss.broadcast(`Current visitors: ${numClients}`)
@@ -44,5 +60,32 @@ wss.broadcast = function broadcast(data)
   wss.clients.forEach(function each(client)
   {
     client.send(data)
+  })
+}
+
+// End websocket
+// Begin database
+
+const sqlite = require('sqlite3')
+const db = new sqlite.Database(':memory:')
+
+db.serialize(() =>
+{
+  db.run(`
+    CREATE TABLE visitors (
+    count INTEGER,
+    time TEXT
+    )`)
+})
+
+function shutdownDB()
+{
+  console.log('Shutting down db')
+  db.each("SELECT * FROM visitors", (err, row) =>
+  {
+    if (!err) console.log(row)
+  }, () =>
+  {
+    db.close(() => process.exit(0))
   })
 }
